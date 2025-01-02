1ee2b900632cc57965f7000a9ab5037f4bb68bdf
Phleggmboi <114324105+Aol-Instant-Messenger@users.noreply.github.com>
2025-01-01 22:05:03 -0800

    Added variables to game.lua

--- game.lua
+++ game.lua
@@ -4,6 +4,102 @@ function invalid()
     print("\nYou entered an invalid character.\nIt has costed you this run.\n")
 end
 
+--- Check if a file or directory exists in this path
+function exists(file)
+   local ok, err, code = os.rename(file, file)
+   if not ok then
+      if code == 13 then
+         -- Permission denied, but it exists
+         return true
+      end
+   end
+   return ok, err
+end
+
+--- Check if a directory exists in this path
+function isdir(path)
+   -- "/" works on both Unix and Windows
+   return exists(path.."/")
+end
+
+
+-- Create characters
+unk = "???: "
+MC = "Me: "
+lead = "Counselor Baldur: "
+S = "Madeline: "
+T = "Natalie: "
+E = "Eleanor: "
+A = "Cordelia: "
+M = "Violet: "
+narrator = ": "
+
+-- Met variables
+meetS = false
+meetT = false
+meetE = false
+meetA = false
+meetM = false
+
+-- Route variables
+sciRoute = false
+techRoute = false
+engRoute = false
+artRoute = false
+mathRoute = false
+routeID = 6
+
+-- Interest variables
+sciInterest = false
+sciUnsure = false
+sciDisinterest = false
+
+techInterest = false
+techUnsure = false
+techDisinterest = false
+
+engInterest = false
+engUnsure = false
+engDisinterest = false
+
+artInterest = false
+artUnsure = false
+artDisinterest = false
+artnot_met = false
+artIndecisive = false
+artPerv = false
+
+mathInterest = false
+mathUnsure = false
+mathDisinterest = false
+
+-- Persistent variables
+if not isdir("/usr/LACYOABBA") then
+    os.execute("mkdir /usr/LACYOABBA")
+end
+
+local persistent = {
+    sciEver = false,
+    techEver = false,
+    engEver = false,
+    artEver = false,
+    mathEver = false,
+    playthrough = 0,
+    beaten = 0,
+    started = 0,
+    firstChosen = "N/A",
+    ending1 = false,
+    ending2 = false,
+    enging3 = false,
+    ending4 = false,
+    ending5 = false,
+    ending6 = false,
+    ending7 = false,
+    ending8 = false,
+    ending9 = false,
+    ending10 = false,
+}
+
 -- clear screen
 os.execute("clear")
 
